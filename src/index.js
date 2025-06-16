9c64bc28ad7eb34375093555d0b85b0614f6ac08
Ivan Ticona <ivan.ticona.v@gmail.com>
2025-06-16 02:20:37 -0400

    Pushed to deploy

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const projectsRoutes = require('./routes/projects');
 const app = express();
 
 app.use(cors({ origin: config.APP.FRONTEND_URL, credentials: true }));
-app.options('*', cors({ origin: config.APP.FRONTEND_URL, credentials: true }));
+app.options('/*', cors({ origin: config.APP.FRONTEND_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
